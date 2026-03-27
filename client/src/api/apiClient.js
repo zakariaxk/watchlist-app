647c8a6e2a523f5b23366f13f69d7106a5074135
Horacio Sierra <perezsierra2499@gmail.com>
2026-03-26 21:42:16 -0400

    changed the api base url

--- client/src/api/apiClient.js
+++ client/src/api/apiClient.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:5001/api';
+const API_BASE_URL = 'http://poosdproject.space:5001/api';
 
 const apiClient = axios.create({
   baseURL: API_BASE_URL,
